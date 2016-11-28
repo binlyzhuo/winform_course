d30a7c5d6969864af9ce528ebe737ae4a9f6d4c9
binlyzhuo <binlyzhuo@outlook.com>
2016-11-28 20:41:20 +0800

    ~

---
+++ Winform_Course/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Winform_Course
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(23, 13);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "button1";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(384, 227);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(23, 286);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(384, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 358);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnOK);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Winform_Course/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Winform_Course
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        private int currentIndex = 0;
+        private int max = 10000;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            btnOK.Enabled = false;
+            Thread thread = new Thread(new ThreadStart(LoadData));
+            thread.Start();
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = max;
+        }
+
+        private void LoadData()
+        {
+            //label1.Text = "loading";
+            SetLabelText("数据加载中......");
+            currentIndex = 1;
+            table = new DataTable();
+            table.Columns.Add("id");
+            table.Columns.Add("name");
+            table.Columns.Add("age");
+
+            while (currentIndex<max)
+            {
+                decimal completed = Convert.ToDecimal(currentIndex)/Convert.ToDecimal(max)*100;
+                SetLabelText(string.Format("当前行:{0},剩余量:{1},完成比例:{2}",currentIndex,max-currentIndex,completed));
+                SetPbValue(currentIndex);
+
+                DataRow dr = table.NewRow();
+                dr["id"] = currentIndex;
+                dr["name"] = "张三";
+                dr["age"] = currentIndex + 5;
+                table.Rows.Add(dr);
+                currentIndex++;
+            }
+
+            SetDgvDataSource(table);
+            SetLabelText("数据加载完成");
+
+            Action action = new Action(delegate()
+            {
+                btnOK.Enabled = true;
+            });
+
+            //btnOK.Enabled = true;
+            //var async= BeginInvoke(new MethodInvoker(delegate()
+            //{
+            //    btnOK.Enabled = true;
+            //}));
+
+            var async = BeginInvoke(action);
+            while (async.IsCompleted==false)
+            {
+                
+            }
+
+            object asyncResult = EndInvoke(async);
+        }
+
+        private delegate void labDelegate(string str);
+
+        private void SetLabelText(string str)
+        {
+            if (label1.InvokeRequired)
+            {
+                Invoke(new labDelegate(SetLabelText), new object[] {str});
+            }
+            else
+            {
+                label1.Text = str;
+            }
+        }
+
+        private delegate void pbDelegate(int value);
+
+        private void SetPbValue(int value)
+        {
+            if (progressBar1.InvokeRequired)
+            {
+                Invoke(new pbDelegate(SetPbValue),new object[] {value});
+            }
+            else
+            {
+                progressBar1.Value = value;
+            }
+        }
+
+        private delegate void dgvDelegate(DataTable table);
+
+        private void SetDgvDataSource(DataTable table)
+        {
+            if (dataGridView1.InvokeRequired)
+            {
+                Invoke(new dgvDelegate(SetDgvDataSource),new object[] {table});
+            }
+            else
+            {
+                dataGridView1.DataSource = table;
+            }
+        }
+    }
+}
