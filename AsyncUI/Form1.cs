dc972fad75b939dffdfd2a0471320d3ccfc431c8
binlyzhuo <binlyzhuo@outlook.com>
2016-11-30 21:19:15 +0800

    ~

---
+++ AsyncUI/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task t = DoSomethingAsync();
+            
+        }
+
+        async Task DoSomethingAsync()
+        {
+            int val = 10;
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            val *= 2;
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            Trace.WriteLine(val);
+        }
+    }
+}
