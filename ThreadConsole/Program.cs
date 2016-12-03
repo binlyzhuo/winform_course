ab7810b836cb643db915f62385363b4fa3fe2ac5
binlyzhuo <binlyzhuo@outlook.com>
2016-12-03 14:29:09 +0800

    ~

---
+++ ThreadConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(PrintNumbers);
+            t.Start();
+            PrintNumbers();
+            Console.WriteLine();
+        }
+
+        static void PrintNumbers()
+        {
+            Console.WriteLine("Starting...");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
