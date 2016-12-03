e75dd968b7c6151a67fe5178df24e137a98accca
binlyzhuo <binlyzhuo@outlook.com>
2016-12-03 20:56:48 +0800

    ~

--- ThreadConsole/Program.cs
+++ ThreadConsole/Program.cs
@@ -10,10 +10,10 @@ namespace ThreadConsole
     {
         static void Main(string[] args)
         {
-            Thread t = new Thread(PrintNumbers);
-            t.Start();
-            PrintNumbers();
-            Console.WriteLine();
+          
+            Console.WriteLine("Starting Program...");
+            ThreadPriorty();
+            Console.ReadLine();
         }
 
         static void PrintNumbers()
@@ -23,6 +23,66 @@ namespace ThreadConsole
             {
                 Console.WriteLine(i);
             }
+        }
+
+
+        static void PrintNumbersWithDelay()
+        {
+            Console.WriteLine("Starting...");
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                Console.WriteLine(i);
+            }
+        }
+
+        static void DoSomehing()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+
+        static void PrintNumberWithStatus()
+        {
+            Console.WriteLine("Starting");
+            Console.WriteLine(Thread.CurrentThread.ThreadState.ToString());
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                Console.WriteLine(i);
+            }
+        }
+
+        static void ThreadStatusMethod()
+        {
+            Thread t = new Thread(PrintNumberWithStatus);
+            Thread t2 = new Thread(DoSomehing);
+            Console.WriteLine(t.ThreadState.ToString());
+            t2.Start();
+            t.Start();
+
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine(t.ThreadState.ToString());
+            }
+
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+            t.Abort();
+            Console.WriteLine("A thread has been aborted!");
+            Console.WriteLine(t.ThreadState.ToString());
+            Console.WriteLine(t2.ThreadState.ToString());
+        }
+
+        static void ThreadPriorty()
+        {
+            Console.WriteLine("Current thread priority :{0}",Thread.CurrentThread.Priority);
+        }
+
+        static void RunThreads()
+        {
+            var sample = new ThreadSample();
+            var threadOne = new Thread(sample.CountNumber);
+            threadOne.Name = "ThreadOne";
         }
     }
 }
---
+++ ThreadConsole/ThreadSample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadConsole
+{
+    public class ThreadSample
+    {
+        private bool _isStopped = false;
+
+        public void Stop()
+        {
+            _isStopped = true;
+        }
+
+        public void CountNumber()
+        {
+            long counter = 0;
+            while (_isStopped)
+            {
+                counter++;
+            }
+
+            Console.WriteLine("{0} with {1,11} " +
+                              "priority has a count={2,3}",
+                              Thread.CurrentThread.Name,
+                              Thread.CurrentThread.Priority,
+                              counter.ToString("N0"));
+        }
+    }
+}
