fb3e91b672946f1334665f050cdd9625db67228e
binlyzhuo <binlyzhuo@outlook.com>
2016-12-04 22:11:38 +0800

    ~

---
+++ IOProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IOProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
