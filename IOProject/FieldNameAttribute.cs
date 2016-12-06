4000105c423f986479cab25b744528ba1902aac4
binlyzhuo <binlyzhuo@outlook.com>
2016-12-06 21:57:23 +0800

    ~

---
+++ IOProject/FieldNameAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IOProject
+{
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple = false,Inherited = false)]
+    public class FieldNameAttribute:Attribute
+    {
+        private string name;
+
+        public FieldNameAttribute(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ IOProject/SocialSecurityNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IOProject
+{
+    
+    public class SocialSecurity
+    {
+        [FieldName("SocialSecurityNumber")]
+        public string SocialSecurityNumber { get; set; }
+    }
+}
