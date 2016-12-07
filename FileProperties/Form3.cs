9154d5464a50c0699f827803058cb4a2032c7984
binlyzhuo <binlyzhuo@outlook.com>
2016-12-07 21:35:03 +0800

    ~

---
+++ FileProperties/Form3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileProperties
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //File.WriteAllText(textBox1.Text,textBox2.Text);
+            string[] movies = {"Grease","Close Encounter of third kind!","The day after tomorrow"};
+            File.WriteAllLines("f:\\3.txt",movies);
+        }
+    }
+}
