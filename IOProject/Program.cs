4acc27218e6ba8007d19b6da1cb9f657a91bb379
binlyzhuo <binlyzhuo@outlook.com>
2016-12-07 22:00:44 +0800

    ~

--- IOProject/Program.cs
+++ IOProject/Program.cs
@@ -20,8 +20,17 @@ namespace IOProject
             //myFile.MoveTo(@"F:\eclipse\new\2.txt");
 
             string p = Path.Combine(@"F:\eclipse\new", "2.txt");
+
+            FileStreamDemo();
             Console.WriteLine("IO Complete!");
             Console.ReadLine();
+        }
+
+        static void FileStreamDemo()
+        {
+            FileStream fs = new FileStream("f:\\1.doc", FileMode.Create);
+            int nextByte = fs.ReadByte();
+            FileStream fs2 = new FileStream("f:\\3.doc", FileMode.Create, FileAccess.Write);
         }
     }
 }
