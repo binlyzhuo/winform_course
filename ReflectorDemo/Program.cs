b114d7c555b5ac9eeeac0dfbb633d7eff2f62905
binlyzhuo <binlyzhuo@outlook.com>
2016-12-13 16:36:22 +0800

    ~

--- ReflectorDemo/Program.cs
+++ ReflectorDemo/Program.cs
@@ -18,13 +18,15 @@ namespace ReflectorDemo
 
 
             Type type = model.GetType();
-            var ps = type.GetProperties();
+            //var ps = type.GetProperties();
 
-            //var ps = type.GetProperties().Where(u=>u.PropertyType.FullName =="System.String").ToList();
+            var ps = type.GetProperties().Where(u=>u.PropertyType.FullName =="System.String").ToList();
 
             foreach (var p in ps)
             {
                 Console.WriteLine("PropertyName:{0},Value:{1}",p.Name,p.GetValue(model,null).ToString());
+                p.SetValue(model, "1001",null);
+                Console.WriteLine("PropertyName:{0},Value:{1}", p.Name, p.GetValue(model, null).ToString());
             }
 
             Console.ReadLine();
