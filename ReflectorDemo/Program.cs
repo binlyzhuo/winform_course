862e6be25e48f05ac8a5500def760c52b90f845d
binlyzhuo <binlyzhuo@outlook.com>
2016-12-13 16:28:51 +0800

    ~

---
+++ ReflectorDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace ReflectorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserSearchRequest model = new UserSearchRequest()
+            {
+                Name = "'1'=0",
+                Age = 10
+            };
+
+
+            Type type = model.GetType();
+            var ps = type.GetProperties();
+
+            //var ps = type.GetProperties().Where(u=>u.PropertyType.FullName =="System.String").ToList();
+
+            foreach (var p in ps)
+            {
+                Console.WriteLine("PropertyName:{0},Value:{1}",p.Name,p.GetValue(model,null).ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class UserSearchRequest
+    {
+        public string Name { set; get; }
+        public int Age { set; get; }
+    }
+}
