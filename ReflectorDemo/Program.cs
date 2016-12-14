3fab2bf53936d27f593db4c209e66322ecf62ad3
binlyzhuo <binlyzhuo@outlook.com>
2016-12-14 10:17:10 +0800

    ~

--- ReflectorDemo/Program.cs
+++ ReflectorDemo/Program.cs
@@ -13,20 +13,25 @@ namespace ReflectorDemo
             UserSearchRequest model = new UserSearchRequest()
             {
                 Name = "'1'=0",
-                Age = 10
+                Age = 10,
+                Params = new BaseParam()
+                {
+                    Keywords = "kk"
+                }
             };
 
 
-            Type type = model.GetType();
+            Type type = model.Params.GetType();
             //var ps = type.GetProperties();
 
             var ps = type.GetProperties().Where(u=>u.PropertyType.FullName =="System.String").ToList();
+            var cs = type.GetProperties();
 
             foreach (var p in ps)
             {
-                Console.WriteLine("PropertyName:{0},Value:{1}",p.Name,p.GetValue(model,null).ToString());
-                p.SetValue(model, "1001",null);
-                Console.WriteLine("PropertyName:{0},Value:{1}", p.Name, p.GetValue(model, null).ToString());
+                Console.WriteLine("PropertyName:{0},Value:{1}",p.Name,p.GetValue(model.Params,null).ToString());
+                p.SetValue(model.Params, "1001",null);
+                Console.WriteLine("PropertyName:{0},Value:{1}", p.Name, p.GetValue(model.Params, null).ToString());
             }
 
             Console.ReadLine();
@@ -37,5 +42,12 @@ namespace ReflectorDemo
     {
         public string Name { set; get; }
         public int Age { set; get; }
+
+        public BaseParam Params { set; get; }
+    }
+
+    public class BaseParam
+    {
+        public string Keywords { set; get; }
     }
 }
