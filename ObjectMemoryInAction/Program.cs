de98a6e32d514ff0d0927754a9b2ab683ab2dd45
binlyzhuo <binlyzhuo@outlook.com>
2016-12-19 17:03:41 +0800

    ~

---
+++ ObjectMemoryInAction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ObjectMemoryInAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Person
+    {
+        public int Id;
+
+        public void Eat()
+        {
+            Console.WriteLine("Eat Pear!");
+        }
+    }
+
+    public class Student : Person
+    {
+        public int studentId;
+
+        public void GotoSchool()
+        {
+            Console.WriteLine("Go to school");
+        }
+    }
+}
