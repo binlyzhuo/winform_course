a768d77dd7bd6f7610b32951dfdd1cdd3ebae2a8
binlyzhuo <binlyzhuo@outlook.com>
2017-01-12 17:00:59 +0800

    commit

--- ClosePackageConsole/Program.cs
+++ ClosePackageConsole/Program.cs
@@ -12,8 +12,30 @@ namespace ClosePackageConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Close package in C#!");
-            ListUtil.Execute();
+            //ListUtil.Execute();
+            Console.WriteLine("Maximum length of string to include?");
+            int maxLength = int.Parse(Console.ReadLine());
+            VariableLengthMather mather = new VariableLengthMather(maxLength);
+            Predicate<string> predicate = mather.Math;
+            IList<string> shortWords = ListUtil.Filter(ListUtil.SampleData(), predicate);
+            //ListUtil.Dump(shortWords);
+
+            Console.WriteLine("Now for words with:");
+            maxLength = 5;
+            shortWords = ListUtil.Filter(ListUtil.SampleData(), predicate);
+            //ListUtil.Dump(shortWords);
+
+            string name = "cnblogs";
+            Func<string> capture = () => name;
+            //Print(capture);
+            name = "bin";
+            Print(capture);
             Console.ReadLine();
+        }
+
+        static void Print(Func<string> f)
+        {
+            Console.WriteLine(f());
         }
     }
 }
