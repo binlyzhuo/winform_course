1c0098ce4c864a89622ef88ecf18f0899e27241e
binlyzhuo <binlyzhuo@outlook.com>
2017-01-13 14:20:15 +0800

    commit

---
+++ ClosePackageConsole/VariableLengthMather.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosePackageConsole
+{
+    public class VariableLengthMather
+    {
+        private int maxLength;
+
+        public VariableLengthMather(int maxLength)
+        {
+            this.maxLength = maxLength;
+        }
+
+        public bool Math(string item)
+        {
+            return item.Length <= maxLength;
+        }
+    }
+}
